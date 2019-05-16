d98ba9e879a74db0e7455cc580006500f88a01e9
YuriyGolinkovskiy <yuragolinkovskiy@gmail.com>
2019-05-16 22:57:13 +0300

    order

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,13 +5,12 @@
         <md-button class="md-icon-button" @click="toggleMenu" v-if="!menuVisible">
           <md-icon>menu</md-icon>
         </md-button>
-        <span class="md-title">My Title</span>
+        <span class="md-title">Order Menu</span>
       </md-app-toolbar>
 
       <md-app-drawer :md-active.sync="menuVisible" md-persistent="full">
         <md-toolbar class="md-transparent" md-elevation="0">
-          <span>My Order</span>
-
+          <h2>Your Order</h2>
           <div class="md-toolbar-section-end">
             <md-button class="md-icon-button md-dense" @click="toggleMenu">
               <md-icon>keyboard_arrow_left</md-icon>
@@ -31,12 +30,13 @@
           </md-list-item>
 
         </md-list>
+        <md-button @click="pay" class="md-accent">Pay: {{orderCost}}₽</md-button>
       </md-app-drawer>
 
       <md-app-content>
        <div>
       <md-card md-with-hover
-      v-for="ingredient in Ingredients"
+      v-for="ingredient in ingredients"
       :key="ingredient.name">
       <md-ripple>   
       <md-card-header>
@@ -46,19 +46,19 @@
         </md-card-header-text>
        
       </md-card-header>
-
          <md-card-media>
           <img :src="`${ingredient.image}`" alt="none" class="img">
         </md-card-media>
-
       <md-card-actions>
         <div class="group">      
             <input type="text" required v-model="ingredient.selected">
-            <span class="bar"></span>
             <label >product weight (g)</label>
         </div>
-        <md-button @click="add(ingredient.id)">Add</md-button>
       </md-card-actions>
+      <div class="buttons">
+          <md-button @click="remove(ingredient.id)" class="md-raised ">Remove</md-button>
+          <md-button @click="add(ingredient.id)" class="md-raised ">Add</md-button>
+      </div>
     </md-ripple>
     </md-card>
   </div>
@@ -72,15 +72,16 @@
     name: 'PersistentMini',
     data: () => ({
       menuVisible: false,
-      Ingredients: [
+      ingredients: [
           {id: 0, name: 'meat', cost: 300, image:'https://images.wallpaperscraft.ru/image/myaso_chesnok_pripravy_petrushka_listiki_tarelka_posuda_belyy_fon_78086_1920x1080.jpg', selected: null},
           {id: 1, name: 'cheese', cost: 200, image:'https://avatars.mds.yandex.net/get-pdb/1101614/72c98b5f-81ce-4e0a-9bae-017f63db8c53/s1200?webp=false', selected: null},
           {id: 2, name: 'mushrooms', cost: 150, image:'https://avatars.mds.yandex.net/get-pdb/1365671/281a3841-0f32-4019-8bad-95502724d14a/s1200', selected: null},
           {id: 3, name: 'vegetables', cost: 230, image:'https://www.belnovosti.by/sites/default/files/article/20-02-2017/f87509fc99815c19e8cc313b5cdacadf.jpg', selected: null},
           {id: 4, name: 'fruits', cost: 350, image:'https://avatars.mds.yandex.net/get-pdb/788379/f795e96d-dbb5-4307-8fb9-47198a70c519/s1200?webp=false', selected: null},
       ],
-      Order:[],
-      list:[]
+      order:[],
+      list:[],
+      orderCost:0,
     }),
     methods: {
       toggleMenu () {
@@ -88,23 +89,44 @@
       },
       add :function (id) {
         this.list = []
-        if(this.Ingredients[id].selected != 0){
-            if (id in this.Order){
-              this.Order[id].selected += Number.parseInt(this.Ingredients[id].selected);
-              this.Order[id].price += this.Ingredients[id].selected * this.Ingredients[id].cost *0.01;
+        if(this.ingredients[id].selected != null){
+            if (id in this.order){
+              this.order[id].selected += Number.parseInt(this.ingredients[id].selected);
+              this.order[id].price += this.ingredients[id].selected * this.ingredients[id].cost *0.01;
             }
             else{
-              this.Order[id] = ({id: this.Ingredients[id].id, selected: Number.parseInt(this.Ingredients[id].selected), price: this.Ingredients[id].selected * this.Ingredients[id].cost *0.01, image: this.Ingredients[id].image});
+              this.order[id] = ({id: this.ingredients[id].id, selected: Number.parseInt(this.ingredients[id].selected), price: this.ingredients[id].selected * this.ingredients[id].cost *0.01, image: this.ingredients[id].image});
             }
         }
-        this.Order.forEach(element => {
+        this.order.forEach(element => {
           if(element != undefined){
             this.list.push(element);
           }
         });
-        // eslint-disable-next-line no-console
-        console.log(this.list);
-        this.Ingredients[id].selected = '';
+        this.orderCost += Number.parseFloat(this.ingredients[id].selected * this.ingredients[id].cost *0.01);
+        this.ingredients[id].selected = null
+      },
+      remove :function (id) {
+        this.list = []
+        if(this.ingredients[id].selected != null){
+            if (id in this.order){
+                this.order[id].selected -= Number.parseInt(this.ingredients[id].selected);
+                this.order[id].price -= this.ingredients[id].selected * this.ingredients[id].cost *0.01;
+                this.orderCost -= Number.parseFloat(this.ingredients[id].selected * this.ingredients[id].cost *0.01);
+            }
+            else{
+              alert('Этот товар еще не был добавлен');
+            }
+        }
+        this.order.forEach(element => {
+          if(element != undefined){
+            this.list.push(element);
+          }
+        });
+        this.ingredients[id].selected = null
+      },
+      pay:function(){
+        alert('Функция в данный момент не доступна:)');
       }
     },
   }
@@ -128,6 +150,7 @@
     margin: 4px;
     display: inline-block;
     vertical-align: top;
+    box-shadow: 0 2px 4px 0 rgba(0,0,0,0.14), 0 3px 4px 0 rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.20);
   }
 .img{
     height: 200px !important;
@@ -139,8 +162,16 @@
     margin-top: 10px;
     margin-bottom: 30px;
   }
-
+.buttons{
+ width: 100%;
+ padding-bottom: 10px;
+}
+.md-raised{
+  width: 40%;
+  box-shadow: 0 16px 24px 2px rgba(0,0,0,0.14), 0 6px 30px 5px rgba(0,0,0,0.12), 0 8px 10px 0 rgba(0,0,0,0.20);
+}
 input {
+  width: 300px;
   font-size: 16px;
   padding: 10px;
   display: block;
@@ -161,43 +192,32 @@ label {
   -moz-transition: 0.2s ease all;
   -webkit-transition: 0.2s ease all;
 }
-
-/* active state */
 input:focus ~ label, input:valid ~ label {
   top: -10px;
   font-size: 14px;
   color: #5264AE;
 }
+.md-drawer.md-active{
+box-shadow: 0 8px 10px 1px rgba(0,0,0,0.14), 0 3px 14px 3px rgba(0,0,0,0.12), 0 4px 5px 0 rgba(0,0,0,0.20);
+}
 
+.md-accent{
+ background: rgb(248, 105, 105);
+ box-shadow: 0 2px 4px 0 rgba(0,0,0,0.14), 0 3px 4px 0 rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.20);
+}
 
-/* BOTTOM BARS ================================= */
-.bar {
-  position: relative;
-  display: block;
+.small-img{
+  height: 100px !important;
+  width: 100%;
 }
-.bar:before, .bar:after {
-  content: "";
-  height: 2px;
-  width: 0;
-  bottom: 0;
-  position: absolute;
-  background: #5264AE;
-  transition: 0.2s ease all;
-  -moz-transition: 0.2s ease all;
-  -webkit-transition: 0.2s ease all;
-}
-.bar:before {
-  left: 50%;
-}
-.bar:after {
-  right: 50%;
+.md-list{
+  width: 90%;
+  margin-left: 5%;
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,0.14), 0 3px 4px 0 rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.20);
+  margin-top: 10px;
 }
 .md-list-item-text{
   text-align: center;
 }
-/* active state */
-input:focus ~ .bar:before,
-input:focus ~ .bar:after {
-  width: 50%;
-}
+
 </style>
